806a1cc2a10bd6622dcda6e285fa6200a0c38901
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2016-10-18 11:57:40 +0200

    Initial commit

---
+++ acumulus.ocmod/upload/admin/controller/module/acumulus.php
@@ -0,0 +1,81 @@
+<?php
+
+use Siel\Acumulus\OpenCart\OpenCart2\Helpers\OcHelper;
+
+/** @noinspection PhpUndefinedClassInspection */
+/**
+ * Class ControllerModuleAcumulus is the Acumulus admin site controller.
+ */
+class ControllerModuleAcumulus extends Controller
+{
+    /** @var \Siel\Acumulus\OpenCart\Helpers\OcHelper */
+    private $ocHelper = null;
+
+    /**
+     * Constructor.
+     *
+     * @param \Registry $registry
+     */
+    public function __construct($registry)
+    {
+        parent::__construct($registry);
+        if ($this->ocHelper === null) {
+            // Load autoloader and then our helper that contains OC1 and OC2
+            // shared code.
+            require_once(DIR_SYSTEM . 'library/Siel/psr4.php');
+            $this->ocHelper = new OcHelper($this->registry, 'OpenCart\OpenCart2');
+        }
+    }
+
+    /**
+     * Install controller action, called when the module is installed.
+     */
+    public function install()
+    {
+        $this->ocHelper->install();
+    }
+
+    /**
+     * Uninstall function, called when the module is uninstalled by an admin.
+     */
+    public function uninstall()
+    {
+        $this->ocHelper->uninstall();
+    }
+
+    /**
+     * Main controller action: show/process the basic settings form for this
+     * module.
+     */
+    public function index()
+    {
+        $this->ocHelper->config();
+    }
+
+    /**
+     * Controller action: show/process the advanced settings form for this
+     * module.
+     */
+    public function advanced()
+    {
+        $this->ocHelper->advancedConfig();
+    }
+
+    /**
+     * Controller action: show/process the batch form for this module.
+     */
+    public function batch()
+    {
+        $this->ocHelper->batch();
+    }
+
+    /**
+     * Explicit confirmation step to allow to retain the settings.
+     *
+     * The normal uninstall action will unconditionally delete all settings.
+     */
+    public function confirmUninstall()
+    {
+        $this->ocHelper->confirmUninstall();
+    }
+}
---
+++ acumulus.ocmod/upload/catalog/controller/module/acumulus.php
@@ -0,0 +1,43 @@
+<?php
+
+use Siel\Acumulus\OpenCart\Helpers\OcHelper;
+
+/** @noinspection PhpUndefinedClassInspection */
+
+/**
+ * Class ControllerModuleAcumulus is the Acumulus catalog site controller.
+ */
+class ControllerModuleAcumulus extends Controller
+{
+    /** @var \Siel\Acumulus\OpenCart\Helpers\OcHelper */
+    private $ocHelper = null;
+
+    /**
+     * Constructor.
+     *
+     * @param \Registry $registry
+     */
+    public function __construct($registry)
+    {
+        parent::__construct($registry);
+        if ($this->ocHelper === null) {
+            // Load autoloader and then our helper that contains OC1 and OC2
+            // and admin and catalog shared code.
+            require_once(DIR_SYSTEM . 'library/Siel/psr4.php');
+            $this->ocHelper = new OcHelper($this->registry, 'OpenCart\OpenCart2');
+        }
+    }
+
+    /**
+     * Event handler that executes on the creation or update of an order.
+     *
+     * @param string $route
+     * @param mixed $output
+     * @param int $order_id
+     * param int $order_status_id
+     */
+    public function eventOrderUpdate($route, $output, $order_id/*, $order_status_id*/)
+    {
+        $this->ocHelper->eventOrderUpdate((int) $order_id);
+    }
+}
